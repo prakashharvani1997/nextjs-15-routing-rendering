78032683b376fc40ed2a362102c25026c4a5d770
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-04 17:27:46 +0530

    added Client component functionality

---
+++ components/nav-link.js
@@ -0,0 +1,16 @@
+
+'use client'
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+export default function NavLink({ href , children }) {
+
+    const path = usePathname()
+
+    return (
+        <Link  className={path.startsWith(href) ?'active' : undefined} href={href}>
+           {children}
+        </Link>
+    )
+}

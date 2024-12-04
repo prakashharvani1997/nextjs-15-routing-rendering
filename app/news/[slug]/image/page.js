ca1dab215bd0819a70235dd3b46b117e55def6d2
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-04 17:45:38 +0530

    nested routes inside dynamic routes

---
+++ app/news/[slug]/image/page.js
@@ -0,0 +1,19 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+
+
+export default function ImagePage({ params }) {
+
+
+    const slug = params.slug;
+    const item = DUMMY_NEWS.find((x)=>(x.slug == slug))
+
+    if(!item){
+        notFound()
+    }
+
+    return (
+        <div className="fullscreen-image">
+        <img src={`/images/news/${item.image}`} alt={item.title} />
+        </div>
+    )
+}
--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,26 +1,32 @@
 import { DUMMY_NEWS } from "@/dummy-news";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
+export default function NewsDetail({ params }) {
+  const slug = params.slug;
+  const item = DUMMY_NEWS.find(x => x.slug == slug);
 
+  if (!item) {
+    notFound();
+  }
+  return (
+    <article className="news-article">
+      <header>
+        <Link href={`/news/${item.slug}/image`}>
+          <img src={`/images/news/${item.image}`} alt={item.title} />
+        </Link>
 
-export default function NewsDetail({params}) {
+        <h1>
+          {item.title}
+        </h1>
+        <time dateTime={item.date}>
+          {" "}{item.date}{" "}
+        </time>
+      </header>
 
-    const slug = params.slug;
-    const item = DUMMY_NEWS.find((x)=>(x.slug == slug))
-
-    if(!item){
-        notFound()
-    }
-    return <article className="news-article">
-        <header >
-        <img src={`/images/news/${item.image}`} alt={item.title} />
-
-        <h1>{item.title}</h1>
-        <time dateTime={item.date}> {item.date} </time>
-        </header>
-
-       <p>
+      <p>
         {item.content}
-        </p>
+      </p>
     </article>
+  );
 }

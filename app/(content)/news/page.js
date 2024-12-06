034c6e65a8f3af7486141bca490facffb10b1aca
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-06 11:45:20 +0530

    integrated server side data fetching.

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,47 +1,16 @@
-'use client';
 import NewsList from "@/components/news-list";
- import { useEffect, useState } from "react";
- 
-export default function NewsPage() {
 
-  const [isLoading,setIsLoading] = useState(false);
-  const [error,setError] = useState();
-  const [news,setNews] = useState([]);
+export default async function NewsPage() {
+  const res = await fetch("http://localhost:8080/news");
 
-  useEffect(()=>{
-
-    async function fetchNews() {
-      setIsLoading(true)
-
-     const res = await fetch('http://localhost:8080/news')
-
-     setIsLoading(false)
-     if(!res.ok){
-      setError('Failed to load the data.')
-     }
-     setNews(await res.json())
-    }
-    fetchNews()
-
-  },[])
-
-  if(isLoading){
-    return <p> Loading....</p>
+  if (!res.ok) {
+    throw new Error("Failed to load the data.");
   }
-  if(error){
-    return <p> {error}</p>
-  }
-
-  let newsContent ;
-
-  if(news){
-    newsContent =  <NewsList news={news} />
-  }
-
+  let news = await res.json()
   return (
     <div>
       <h1>News</h1>
-     {newsContent}
+      <NewsList news={news} />
     </div>
   );
 }
